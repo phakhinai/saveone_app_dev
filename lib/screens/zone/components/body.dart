227f0d01e8c441f5394ff17504a66c14d73f1f5c
m.phakhinai <m.phakhinai@outlook.co.th>
2021-05-30 22:36:43 +0700

    fixed error

--- lib/screens/zone/components/body.dart
+++ lib/screens/zone/components/body.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:saveone_app/constants.dart';
-import 'package:saveone_app/screens/zone/components/grid_zone.dart';
-
 import 'info.dart';
 
 class Body extends StatelessWidget {
@@ -9,35 +6,11 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          Info(
-            image: "assets/images/pic.png",
-            name: "นายตลาดเซฟวัน จำกัดจริง",
-            email: "saveone@gmail.com",
-          ),
-          SizedBox(
-            height: 500,
-            child: GridView.count(
-              padding: const EdgeInsets.all(20),
-              crossAxisCount: 2,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
-              children: <Widget>[
-                Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                    color: kBorderColor,
-                    width: 1,
-                  )),
-                ),
-              ],
-            ),
-          ),
-          // GridZone(),
-        ],
+      child: Info(
+        image: "assets/images/pic.png",
+        name: "นายตลาดเซฟวัน จำกัดจริง",
+        email: "saveone@gmail.com",
       ),
     );
   }
